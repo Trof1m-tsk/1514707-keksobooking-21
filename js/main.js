76cc9b9177f9486f89334533a196fe4e7a36fc1b
Trofim-tsk <m.trofimov1987@gmail.com>
2020-10-01 23:29:41 +0700

    Задание личного проекта: Больше деталей выполнено и устранены замечания

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 "use strict";
 
-const OBJECTS_NUMBER = 8;
+const OFFERS_NUMBER = 8;
 const PIN_X_MAX = 1200;
 const PIN_Y_MIN = 130;
 const PIN_Y_MAX = 630;
+const PIN_OFFSET_X = 31;
+const PIN_OFFSET_Y = 84;
 const MAX_PRICE = 100000;
 const MAX_ROOMS = 10;
 const MAX_GUESTS = 20;
@@ -23,54 +25,59 @@ const getRandomNumber = function (number) {
   return Math.floor(Math.random() * number);
 };
 
-const createRandomSizeArray = function (array) {
-  const randomSizeArray = [];
+const createRandomSizeSet = function (array) {
+  const randomSizeSet = new Set();
   const size = 1 + getRandomNumber(array.length);
 
   for (let i = 0; i < size; i++) {
-    const arrayItem = array[getRandomNumber(array.length)];
-    if (!randomSizeArray.includes(arrayItem)) {
-      randomSizeArray.push(arrayItem);
-    }
+    const setItem = array[getRandomNumber(array.length)];
+    randomSizeSet.add(setItem);
   }
 
-  return randomSizeArray;
+  return randomSizeSet;
 };
 
-const generateObjectsArray = function (number) {
-  const createdObjectsArray = [];
+const generateOffer = function (offerIndex) {
+  const obj = {};
+  const avatar = `img/avatars/user0` + (offerIndex + 1) + `.png`;
+  obj.author = {};
+  obj.author.avatar = avatar;
+  obj.location = {};
+  obj.location.x = getRandomNumber(PIN_X_MAX);
+  obj.location.y = PIN_Y_MIN + getRandomNumber(PIN_Y_MAX - PIN_Y_MIN);
+  obj.offer = {};
+  obj.offer.title = `Заголовок предложения`;
+  obj.offer.address = obj.location.x + `, ` + obj.location.y;
+  obj.offer.price = getRandomNumber(MAX_PRICE);
+  obj.offer.type = PROPERTY_TYPES[getRandomNumber(PROPERTY_TYPES.length)];
+  obj.offer.rooms = 1 + getRandomNumber(MAX_ROOMS);
+  obj.offer.guests = 1 + getRandomNumber(MAX_GUESTS);
+  obj.offer.checkin = CHECKIN_HOURS[getRandomNumber(CHECKIN_HOURS.length)];
+  obj.offer.checkout = CHECKOUT_HOURS[getRandomNumber(CHECKOUT_HOURS.length)];
+  obj.offer.features = createRandomSizeSet(FEATURES);
+  obj.offer.photos = createRandomSizeSet(PHOTOS);
+
+  return obj;
+};
+
+const createOffersArray = function (number) {
+  const createdOffersArray = [];
 
   for (let i = 0; i < number; i++) {
-    const obj = {};
-    obj.author = {};
-    obj.author.avatar = `img/avatars/user0` + (i + 1) + `.png`;
-    obj.location = {};
-    obj.location.x = getRandomNumber(PIN_X_MAX);
-    obj.location.y = PIN_Y_MIN + getRandomNumber(PIN_Y_MAX - PIN_Y_MIN);
-    obj.offer = {};
-    obj.offer.title = `Заголовок предложения`;
-    obj.offer.address = obj.location.x + `, ` + obj.location.y;
-    obj.offer.price = getRandomNumber(MAX_PRICE);
-    obj.offer.type = PROPERTY_TYPES[getRandomNumber(PROPERTY_TYPES.length)];
-    obj.offer.rooms = 1 + getRandomNumber(MAX_ROOMS);
-    obj.offer.guests = 1 + getRandomNumber(MAX_GUESTS);
-    obj.offer.checkin = CHECKIN_HOURS[getRandomNumber(CHECKIN_HOURS.length)];
-    obj.offer.checkout = CHECKOUT_HOURS[getRandomNumber(CHECKOUT_HOURS.length)];
-    obj.offer.features = createRandomSizeArray(FEATURES);
-    obj.offer.photos = createRandomSizeArray(PHOTOS);
-    createdObjectsArray.push(obj);
+    createdOffersArray.push(generateOffer(i));
   }
 
-  return createdObjectsArray;
+  return createdOffersArray;
 };
 
-const objectsArray = generateObjectsArray(OBJECTS_NUMBER);
+const offersArray = createOffersArray(OFFERS_NUMBER);
 
 const renderPinElement = function (offerData) {
   const pinElement = pinTemplate.cloneNode(true);
   const pinImage = pinElement.querySelector(`img`);
 
-  pinElement.style = `left: ` + (offerData.location.x - 31) + `px; top: ` + (offerData.location.y - 84) + `px;`;
+  pinElement.style = `left: ` + (offerData.location.x - PIN_OFFSET_X) +
+  `px; top: ` + (offerData.location.y - PIN_OFFSET_Y) + `px;`;
   pinImage.src = offerData.author.avatar;
   pinImage.alt = offerData.offer.title;
 
@@ -80,10 +87,8 @@ const renderPinElement = function (offerData) {
 const renderPinsOnMap = function (dataArray) {
   const pinsFragment = document.createDocumentFragment();
 
-  for (let i = 0; i < dataArray.length; i++) {
-    pinsFragment.appendChild(renderPinElement(dataArray[i]));
-  }
+  dataArray.forEach((el) => {pinsFragment.appendChild(renderPinElement(el))});
   pinsList.appendChild(pinsFragment);
 };
 
-renderPinsOnMap(objectsArray);
+renderPinsOnMap(offersArray);
